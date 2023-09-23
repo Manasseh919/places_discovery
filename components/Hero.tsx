695cecbecb10704f472e04b48fb7e6a2d85522c0
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-09-23 08:58:42 +0000

    categories done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import category from "@/data/category";
 import Image from "next/image";
 import React from "react";
 
@@ -21,9 +22,9 @@ function Hero() {
             <input
               type="text"
               placeholder="Search anything"
-              className=" bg-white p-3 border-[1px] z-20 rounded-full px-5 w-[36%] shadow-sm"
+              className=" bg-white p-3 border-[1px] z-20 rounded-full px-5 w-[36%] shadow-sm outline-red-600"
             />
-            <button className="bg-red-600 rounded-full p-3 shadow-md z-10 cursor-pointer hover:scale-105">
+            <button className="bg-red-600 rounded-full p-3 shadow-md z-10 cursor-pointer hover:scale-105 transition-all">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -39,6 +40,21 @@ function Hero() {
                 />
               </svg>
             </button>
+          </div>
+          <div className="mt-5 flex flex-col justify-center items-center">
+            <h2>Or Browse the category</h2>
+            <div className="grid grid-cols-3 md:grid-cols-7 w-[50%] justify-center gap-5 mt-3">
+              {category.map((item, index) => (
+                <div className="border-[1px] w-[60px] p-4 bg-white rounded-full z-10 hover:border-red-600 hover:scale-110 cursor-pointer transition-all">
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    height={30}
+                    width={30}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
